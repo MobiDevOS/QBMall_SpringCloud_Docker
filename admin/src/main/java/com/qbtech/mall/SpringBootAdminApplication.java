daece3cd8ce6726d54bc985ef48aa0795cb254af
zhujohnle <545948101@qq.com>
2019-05-31 17:49:12 +0800

    添加config 配置中心和eureka注册中心两个配置

--- admin/src/main/java/com/qbtech/mall/SpringBootAdminApplication.java
+++ admin/src/main/java/com/qbtech/mall/SpringBootAdminApplication.java
@@ -1,9 +1,11 @@
 package com.qbtech.mall;
 
+import org.springframework.cloud.client.SpringCloudApplication;
+
 /**
  * 对应最核心业务层
- *
  * */
+@SpringCloudApplication
 public class SpringBootAdminApplication {
 
 }
